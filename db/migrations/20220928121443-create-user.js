b667f8345ee92e7f076b661dddd25f7530577cbd
karenabramyan <ptp.abramyan@gmail.com>
2022-09-28 15:45:25 +0300

    db create

---
+++ db/migrations/20220928121443-create-user.js
@@ -0,0 +1,43 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      phone: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      isAdmin: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  },
+};
