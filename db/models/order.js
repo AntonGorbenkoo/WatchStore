ed494edd1ac801c4e31862226606048adc700673
karenabramyan <ptp.abramyan@gmail.com>
2022-09-28 18:28:02 +0300

    Login.jsx done

--- db/models/order.js
+++ db/models/order.js
@@ -1,6 +1,7 @@
 const {
-  Model
+  Model,
 } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Order extends Model {
     /**
@@ -8,47 +9,51 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({User}) {
-      Order.belongsTo(User, {foreignKey: 'user_id'});
+    static associate({ User }) {
+      Order.belongsTo(User, { foreignKey: 'user_id' });
     }
   }
-  Order.init({
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER
+  Order.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      description: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      img: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      user_id: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
     },
-    title: {
-      allowNull: false,
-      type: DataTypes.TEXT
+    {
+      sequelize,
+      modelName: 'Order',
     },
-    description: {
-      allowNull: false,
-      type: DataTypes.TEXT
-    },
-    img: {
-      allowNull: false,
-      type: DataTypes.TEXT
-    },
-    user_id: {
-      allowNull: false,
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'Users',
-        key: 'id',
-      }
-    },
-    createdAt: {
-      allowNull: false,
-      type: DataTypes.DATE
-    },
-    updatedAt: {
-      allowNull: false,
-      type: DataTypes.DATE
-    }, {
-    sequelize,
-    modelName: 'Order',
-  });
+  );
   return Order;
 };
