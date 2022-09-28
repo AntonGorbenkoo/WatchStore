77d0d6983bdf196c22903e017bc1ce29aa73891e
pustot <maki.nwd@gmail.com>
2022-09-28 18:29:22 +0300

    add css for headers and main page

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const config = require('./config/config');
 
+const mainRouter = require('./routes/main.route');
+
 const PORT = process.env.PORT ?? 3000;
 const app = express();
 config(app);
+
+app.use('/', mainRouter);
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT} port`);
