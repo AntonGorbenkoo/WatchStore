d96afb344d5b6d33876e405059f06705219efe84
karenabramyan <ptp.abramyan@gmail.com>
2022-09-28 18:31:32 +0300

    fix in registrate and login jsx

--- views/regLog/Login.jsx
+++ views/regLog/Login.jsx
@@ -1,25 +1,25 @@
 const React = require('react');
 const Layout = require('../Layout');
 
-module.exports = function Login ({title}) {
-    return (
-        <Layout title={title}>
-            <section className="autherisation">
+module.exports = function Login({ title }) {
+  return (
+    <Layout title={title}>
+      <section className="autherisation">
         <div className="container">
           <h2 className="title">Авторизация</h2>
           <form action="/login" method="post" className="form auth-form">
 
             <label className="form-label" htmlFor="logo-mail">Введите e-mail</label>
-            <input className="form-input input-mail" required type="e-mail" name="mail" placeholder="Введите e-mail" id="logo-mail"/>
+            <input className="form-input input-mail" required type="e-mail" name="mail" placeholder="Введите e-mail" id="logo-mail" />
 
             <label className="form-label" htmlFor="logo-pass-rep">Введите пароль</label>
             <input className="form-input" required type="password" name="password" placeholder="Введите пароль" id="logo-pas-rep" />
 
             <button className="button" type="submit">Войти</button>
-            <div id="messageLog" className='error'/>
+            <div id="messageLog" className="error" />
           </form>
         </div>
-      </section> 
-        </Layout>
-    )
+      </section>
+    </Layout>
+  );
 };
--- views/regLog/Registrate.jsx
+++ views/regLog/Registrate.jsx
@@ -1,51 +1,55 @@
 const React = require('react');
 const Layout = require('../Layout');
 
-module.exports = function Registration ({title}) {
-    return (
-        <Layout title ={title}>
-             <section className="registration">
-      <div className="container">
-        <h2 className="form-title title reg">Регистрация</h2>
-        <form action="/registration" method="post" className="form registration-form">
-          <label className="form-label" htmlFor="reg-name">Введите имя</label>
-          <input className="form-input" type="text" name="name" placeholder="Введите имя" id="reg-name" />
+module.exports = function Registration({ title }) {
+  return (
+    <Layout title={title}>
+      <section className="registration">
+        <div className="container">
+          <h2 className="form-title title reg">Регистрация</h2>
+          <form action="/registration" method="post" className="form registration-form">
+            <label className="form-label" htmlFor="reg-name">Введите имя</label>
+            <input className="form-input" type="text" name="name" placeholder="Введите имя" id="reg-name" />
 
-          <label className="form-label" htmlFor="reg-mail">Введите e-mail</label>
-          <input className="form-input input-mail" required type="email" name="mail" placeholder="mail@mail.com"  id="reg-mail"></input> 
+            <label className="form-label" htmlFor="reg-mail">Введите e-mail</label>
+            <input className="form-input input-mail" required type="email" name="mail" placeholder="mail@mail.com" id="reg-mail" />
 
-          <label className="form-label" htmlFor="reg-pass"> 
-            Придумайте пароль <br />
-            
-          </label>
-          <input
-            className="form-input"
-            required
-            type="password"
-            name="password"
-            placeholder="Придумайте пароль"
-            id="reg-pass"
-            pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
-          />
+            <label className="form-label" htmlFor="reg-tel">Введите телефон</label>
+            <input className="form-input input-tel" required type="tel" name="tel" placeholder="+7(999)1230000"  id="reg-tel" pattern="\+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}"></input> 
+            <label className="form-label" htmlFor="reg-pass">
+              Придумайте пароль
+              {' '}
+              <br />
 
-          <label className="form-label" htmlFor="reg-pass-rep">Повторите пароль</label>
+            </label>
+            <input
+              className="form-input"
+              required
+              type="password"
+              name="password"
+              placeholder="Придумайте пароль"
+              id="reg-pass"
+              pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
+            />
 
-          <input
-            className="form-input"
-            required type="password"
-            name="passwordRepeat"
-            placeholder="Повторите пароль"
-            id="reg-pas-rep"
-          />
+            <label className="form-label" htmlFor="reg-pass-rep">Повторите пароль</label>
 
-          <button type="submit" className="button">Зарегистрироваться</button>
-        <div id="messageReg" className='error'/> 
-        </form>
-      </div>
-    </section>
+            <input
+              className="form-input"
+              required
+              type="password"
+              name="passwordRepeat"
+              placeholder="Повторите пароль"
+              id="reg-pas-rep"
+            />
 
+            <button type="submit" className="button">Зарегистрироваться</button>
+            <div id="messageReg" className="error" />
+          </form>
+        </div>
+      </section>
 
-        </Layout>
+    </Layout>
 
-    )
-}
+  );
+};
