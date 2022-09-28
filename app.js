20d7d895f05b6d4d9ff2f6ba21707030cc963557
Denis <AntonGorbenko7@gmail.com>
2022-09-28 12:11:25 +0300

    init done

---
+++ app.js
@@ -0,0 +1,12 @@
+require('@babel/register');
+require('dotenv').config();
+const express = require('express');
+const config = require('./config/config');
+
+const PORT = process.env.PORT ?? 3000;
+const app = express();
+config(app);
+
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT} port`);
+});
